cb036c3228324108f6e1540c099e0efc91f397bd
Filip Hesse <Filip_Hesse@yahoo.de>
2020-02-21 12:32:08 +0100

    debug

--- catkin_ws/src/labeled_slam/src/activator1.cpp
+++ catkin_ws/src/labeled_slam/src/activator1.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
-#include "std_srvs.h"
+#include "std_srvs/SetBool.h"
 #include "geometry_msgs/Twist.h"
 
 using namespace std;
@@ -10,54 +10,54 @@ using namespace std;
 //Constants and general stuff
 geometry_msgs::Twist velocity_to_publish;
 bool activation = false;
-void velocity_callback(const geometry_msgs::::ConstPtr& received_velocity);
-bool path_following_activate(std_srvs::SetBool& req, std_srvs::SetBool& response);
+void velocity_callback(const geometry_msgs::Twist::ConstPtr& received_velocity);
+bool path_following_activate(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& response);
 
 
 
 int main(int argc, char** argv){
 
-  ros::init(argc, argv, "activator1");
-  ros::Rate loop_rate(1000);
+        ros::init(argc, argv, "activator1");
+        ros::Rate loop_rate(1000);
 
-  //initializing my node
+        //initializing my node
 
-  ros::NodeHandle node;
-  ros::Publisher  twist_pub = node.advertise<geometry_msgs::Twist>("ac1/cmd_vel", 1000); //publisher for the veolocity forwarder/the robot
-  ros::Subscriber twist_sub = node.subscribe("path/cmd_vel", 1000, &velocity_callback); //subscriber for the velocity from the path planner
-  ros::ServiceServer bool_serv = node.advertiseService("activate_path_following", path_following_activate);  //boolean check to see wether data needs to be sent or not.
+        ros::NodeHandle node;
+        ros::Publisher twist_pub = node.advertise<geometry_msgs::Twist>("ac1/cmd_vel", 1000); //publisher for the veolocity forwarder/the robot
+        ros::Subscriber twist_sub = node.subscribe("path/cmd_vel", 1000, &velocity_callback); //subscriber for the velocity from the path planner
+        ros::ServiceServer bool_serv = node.advertiseService("activate_path_following", path_following_activate); //boolean check to see wether data needs to be sent or not.
 
 
 // while here dunno why
-  while(ros::ok()){
-    if(activation == true){
-      Twist_pub.publish(velocity_to_publish);
-      ROS_INFO("Path Follower is active.\n");
-    } else{
-      ROS_INFO("Path Follower is not active.\n");
-    }
+        while(ros::ok()) {
+                if(activation == true) {
+                        twist_pub.publish(velocity_to_publish);
+                        ROS_INFO("Path Follower is active.\n");
+                } else{
+                        ROS_INFO("Path Follower is not active.\n");
+                }
 
-  ros::spin();
-  }
+                ros::spin();
+        }
 
-  return 0;
+        return 0;
 };
 
 
-bool path_following_activate(std_srvs::SetBool& req, std_srvs::SetBool& response){
+bool path_following_activate(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& response){
 
-      activation  == req->data ; // iff activation->data = true
-      response->success = true;
-      response->message = "";
-      return true;
+        activation  == req.data;// iff activation->data = true
+        response.success = true;
+        response.message = "";
+        return true;
 
 }
 
-void velocity_calback(const geometry_msgs::Twist::ConstPtr& received_velocity){
-    velocity_to_publish.linear.x = received_velocity->linear.x;
-    velocity_to_publish.linear.y = received_velocity->linear.y;
-    velocity_to_publish.linear.z = received_velocity->linear.z;
-    velocity_to_publish.angular.x = received_velocity->angular.x;
-    velocity_to_publish.angular.y = received_velocity->angular.y;
-    velocity_to_publish.angular.z = received_velocity->angular.z;
+void velocity_callback(const geometry_msgs::Twist::ConstPtr& received_velocity){
+        velocity_to_publish.linear.x = received_velocity->linear.x;
+        velocity_to_publish.linear.y = received_velocity->linear.y;
+        velocity_to_publish.linear.z = received_velocity->linear.z;
+        velocity_to_publish.angular.x = received_velocity->angular.x;
+        velocity_to_publish.angular.y = received_velocity->angular.y;
+        velocity_to_publish.angular.z = received_velocity->angular.z;
 }
--- catkin_ws/src/labeled_slam/src/activator2.cpp
+++ catkin_ws/src/labeled_slam/src/activator2.cpp
@@ -1,8 +1,8 @@
-m>
+#include <sstream>
 #include <stdlib.h>
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
-#include "std_srvs.h"
+#include "std_srvs/SetBool.h"
 #include "geometry_msgs/Twist.h"
 
 using namespace std;
@@ -10,55 +10,56 @@ using namespace std;
 //Constants and general stuff
 geometry_msgs::Twist velocity_to_publish;
 bool activation = false;
-void velocity_callback(const geometry_msgs::Twist& received_velocity);
-bool driving_activate(std_srvs::SetBool& req, std_srvs::SetBool& response);
+void velocity_callback(const geometry_msgs::Twist::ConstPtr& received_velocity);
+bool driving_activate(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& response);
 
 
 
 int main(int argc, char** argv){
 
-  ros::init(argc, argv, "activator1");
-  ros::Rate loop_rate(1000);
+        ros::init(argc, argv, "activator1");
+        ros::Rate loop_rate(1000);
 
-  //initializing my node
+        //initializing my node
 
-  ros::NodeHandle node;
-  ros::Publisher  twist_pub = node.advertise<geometry_msgs::Twist>("ac1/cmd_vel", 1000); //publisher for the veolocity forwarder/the robot
-  ros::Subscriber twist_sub = node.subscribe("gbc/cmd_vel", 1000, &velocity_callback); //subscriber for the velocity from the path planner
-  ros::ServiceServer bool_serv = node.advertiseService("activate_driving", driving_activate);  //boolean check to see wether data needs to be sent or not.
+        ros::NodeHandle node;
+        ros::Publisher twist_pub = node.advertise<geometry_msgs::Twist>("ac1/cmd_vel", 1000); //publisher for the veolocity forwarder/the robot
+        ros::Subscriber twist_sub = node.subscribe("gbc/cmd_vel", 1000, &velocity_callback); //subscriber for the velocity from the path planner
+        ros::ServiceServer bool_serv = node.advertiseService("activate_driving", driving_activate); //boolean check to see wether data needs to be sent or not.
 
 
 // while here dunno why
-  while(ros::ok()){
-    if(activation == true){
-      Twist_pub.publish(velocity_to_publish);
-      ROS_INFO("Driving Mode is active.\n");
-    } else{
-      ROS_INFO("Driving Mode  is not active.\n");
-    }
+        while(ros::ok()) {
+                if(activation == true) {
+                        twist_pub.publish(velocity_to_publish);
+                        ROS_INFO("Driving Mode is active.\n");
+                } else{
+                        ROS_INFO("Driving Mode  is not active.\n");
+                }
 
-  ros::spin();
-  }
+                ros::spin();
+        }
 
-  return 0;
+        return 0;
 };
 
 
-void driving_callback(const std_msgs::Bool::ConstPtr& activation_msg){
+bool driving_activate(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& response)
+{
 
-  activation  == req->data ; // iff activation->data = true
-  response->success = true;
-  response->message = "";
-  return true;
+        activation  == req.data;// iff activation->data = true
+        response.success = true;
+        response.message = "";
+        return true;
 
 }
 
-void velocity_calback(const geometry_msgs::Twist& received_velocity){
+void velocity_callback(const geometry_msgs::Twist::ConstPtr& received_velocity){
 
-    velocity_to_publish.linear.x = received_velocity->linear.x;
-    velocity_to_publish.linear.y = received_velocity->linear.y;
-    velocity_to_publish.linear.z = received_velocity->linear.z;
-    velocity_to_publish.angular.x = received_velocity->angular.x;
-    velocity_to_publish.angular.y = received_velocity->angular.y;
-    velocity_to_publish.angular.z = received_velocity->angular.z;
+        velocity_to_publish.linear.x = received_velocity->linear.x;
+        velocity_to_publish.linear.y = received_velocity->linear.y;
+        velocity_to_publish.linear.z = received_velocity->linear.z;
+        velocity_to_publish.angular.x = received_velocity->angular.x;
+        velocity_to_publish.angular.y = received_velocity->angular.y;
+        velocity_to_publish.angular.z = received_velocity->angular.z;
 }
